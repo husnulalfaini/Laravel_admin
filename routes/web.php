9798592f9d04427b6e329a405bbe515be0ef4fc5
Husnul Alfaini <husnulalfaini3@gmail.com>
2020-10-14 00:55:50 +0700

    tampilan ui master, dashboard, bottom, top, navigation , header ok

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin'], function(){
+    route::get('/', 'Admin\DashboardController@index');
+});
